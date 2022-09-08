a926345e5a08f1c39e9b632ae5f43b35dcefdbba
WEB LDSP <web_ldsp@sanchezpolo.com>
2022-09-08 03:32:03 -0500

    dashboard depurate and avatar changed

--- src/app/modules/account/account.component.ts
+++ src/app/modules/account/account.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Account } from 'src/app/models/account';
+import { Constant } from 'src/app/shared/constant';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
 })
 export class AccountComponent implements OnInit {
+
+  user:Account;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.user=Constant.AUTH.getUser();
+  }
 }
